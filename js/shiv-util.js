9d127dd5239934de9bec8b1b81d54961f5edb1a9
Michiel Kalkman <michiel@etryx.com>
2010-07-26 22:39:07 +0200

    Added map() and memo() to util

--- js/shiv-util.js
+++ js/shiv-util.js
@@ -12,3 +12,19 @@ shiv.hitch = function(scope, func) {
   };
 }
 
+shiv.map = function(arr, func) {
+  var ret = [];
+  for (var i=0; i<arr.length; i++) {
+    ret.push(func(arr[i]));
+  }
+  return ret;
+}
+
+shiv.memo = function(arr, func) {
+  var mem = null;
+  for (var i=0; i<arr.length; i++) {
+    func(arr[i], mem);
+  }
+  return mem;
+}
+
